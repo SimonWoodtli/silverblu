bce597d24b58a308b463c9ba659d5a222a873fe7
SimonWoodtli <me@simonwoodtli.com>
2026-06-30 18:39:34 +0200

    fix coppr

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,8 @@ dnf5 install -y tmux
 ## https://protonvpn.com/support/official-linux-vpn-fedora
 wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.4-1.noarch.rpm"
 dnf5 -y install ./protonvpn-stable-release-1.0.4-1.noarch.rpm && sudo dnf check-update --refresh
-dnf5 -y install proton-vpn-cli
+dnf5 -y install proton-vpn-gnome-desktop
+
 ##install wezterm:
 dnf5 -y copr enable wezfurlong/wezterm-nightly
 dnf5 -y install wezterm
